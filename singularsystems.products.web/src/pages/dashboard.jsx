875aaeff4ff3c0a71c65d62cb937dfa4542f0a8f
Menzi Manqele <manqele@live.com>
2024-03-09 23:23:29 +0200

    scoffolding pages dashboard and product list

---
+++ singularsystems.products.web/src/pages/dashboard.jsx
@@ -0,0 +1,8 @@
+
+export function Dashboard() {
+    return (
+        <h1>
+             Welcome to Singular Systems Products Dashboard
+        </h1>
+    )
+}
---
+++ singularsystems.products.web/src/pages/products/productsList/productList.jsx
@@ -0,0 +1,5 @@
+export function ProductList(){
+    return(
+        <h1>Product list</h1>
+    )
+};
