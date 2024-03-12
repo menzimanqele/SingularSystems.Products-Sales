e43399c30c505b513b2f93f864e1a3a20c0cffab
Menzi Manqele <manqele@live.com>
2024-03-12 06:37:52 +0200

    route keys

--- singularsystems.products.web/src/routing/AppRoutes.js
+++ singularsystems.products.web/src/routing/AppRoutes.js
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import { Dashboard } from "../pages/dashboard";
 import { ProductList } from "../pages/products/productList/productList";
-import { productListPath } from "./navigationPaths";
+import { productListPath, productSalesPath } from "./navigationPaths";
+import { ProductSales } from "../pages/products/productSales/productSales";
 
 
 export function AppRoutes() {
@@ -10,8 +11,9 @@ export function AppRoutes() {
 
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Dashboard />}></Route>
-      <Route path={productListPath} element={<ProductList />}></Route>
+      <Route key="dashboard" path="/" element={<Dashboard />}></Route>
+      <Route key="productList" path={productListPath} element={<ProductList />}></Route>
+      <Route key="productSales" path={productSalesPath} element={<ProductSales />}></Route>
     </Routes>
   </BrowserRouter>
   );
